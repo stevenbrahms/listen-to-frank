ad07f88c33d38291e7a55b1be583a026c2794fe0
stevenbrahms <stevenbrahms@gmail.com>
2026-06-16 11:46:43 -0400

    Bump cache to frank-v115 (Cartier-Bresson re-record)

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
  *      iOS will refuse to play a cached MP3 that doesn't honor Range.
  */
 
-const CACHE_VERSION = 'frank-v114';
+const CACHE_VERSION = 'frank-v115';
 const SHELL_CACHE = CACHE_VERSION + '-shell';
 const AUDIO_CACHE = CACHE_VERSION + '-audio';
 const VENDOR_CACHE = CACHE_VERSION + '-vendor';
